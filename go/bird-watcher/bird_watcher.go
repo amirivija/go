8bb33edd785ccadac1463b459fb3a177db2a79a1
Vijay Amirisetty <amirisetty.vijayaraghavan@gmail.com>
2023-05-07 14:26:15 -0500

    Solved the bird watcher problem. all Tests Passed

--- go/bird-watcher/bird_watcher.go
+++ go/bird-watcher/bird_watcher.go
@@ -14,11 +14,27 @@ func TotalBirdCount(birdsPerDay []int) int {
 // BirdsInWeek returns the total bird count by summing
 // only the items belonging to the given week.
 func BirdsInWeek(birdsPerDay []int, week int) int {
-	panic("Please implement the BirdsInWeek() function")
+	totalWeeks := len(birdsPerDay) / 7
+
+	if week <= 0 || week > totalWeeks {
+		panic("week is less than 0 or greater than total weeks")
+	}
+
+	birdsInWeek := 0
+
+	for i := (week - 1) * 7; i < (week-1)*7+7; i++ {
+		birdsInWeek += birdsPerDay[i]
+	}
+
+	return birdsInWeek
 }
 
 // FixBirdCountLog returns the bird counts after correcting
 // the bird counts for alternate days.
 func FixBirdCountLog(birdsPerDay []int) []int {
-	panic("Please implement the FixBirdCountLog() function")
+	for i := 0; i < len(birdsPerDay); i += 2 {
+		birdsPerDay[i]++
+	}
+
+	return birdsPerDay
 }
